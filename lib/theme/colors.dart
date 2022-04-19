9979414869c36364f3a0a5cdc0d542d2a9c0a4a0
Eliezer Basubi <eliezer.basubi30@gmail.com>
2022-04-19 08:27:12 +0300

    Build onboarding activity

---
+++ lib/theme/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ThemeColors {
+  final primary = const Color(0xff1d3557);
+  final secondary = const Color(0xff7DFFF3);
+  final tertiary = const Color(0xff9E80FF);
+  final black50 = const Color.fromARGB(0, 0, 0, 5);
+  final green = const Color(0x0002F48B);
+  final red = const Color(0xffe63946);
+  final gray = const Color(0xfff6f6f6);
+  final gray10 = const Color(0xffDADADA);
+
+  const ThemeColors();
+}
